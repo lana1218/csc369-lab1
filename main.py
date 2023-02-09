bf03c474ae77fdf0935fc70b3530320fbcf22bf6
lana1218 <66749266+lana1218@users.noreply.github.com>
2023-02-09 12:51:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,6 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 def main():
-    # data = []
-    # with open('2021-04-03-15.json', 'r', encoding='utf-8') as f:
-    #     for line in f:
-    #         data.append(json.loads(line))
-    #     print(json.dumps(data[0], indent=3))
-    #     print(json.dumps(data[-1], indent=3))
-    #
-    # ids = []
-    # for i in data:
-    #     ids.append(int(i['id']))
-    #
-    # parts = list(chunks(ids, 13431))    #13431 is hard coded and will return 8 chunks
-    #
-    # for i in range(8):
-    #     with open(F'partition0{i}.json', 'w', encoding='utf-8') as f:
-    #         json.dump(data, f, ensure_ascii=False, indent=4)
 
     test_read = []
     for i in range(8):
